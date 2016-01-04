6bba45d65b4a99380b7698d3c7b511ab93cb74da
Leo9012 <2334888094@qq.com>
2016-01-04 09:40:36 +0100

    UI function
    actions on card click is working

--- TheFool/TheFool/Form1.cs
+++ TheFool/TheFool/Form1.cs
@@ -12,11 +12,13 @@ namespace TheFool
 {
     public partial class Form1 : Form
     {
+        public List<PictureBox> pictureBoxList;
         public Form1()
         {
             InitializeComponent();
             this.panelGame.Visible = false;
             this.panelMain.Visible = true;
+            this.pictureBoxList = new List<PictureBox>();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -37,6 +39,100 @@ namespace TheFool
         private void btnPlay_Click_1(object sender, EventArgs e)
         {
             this.panelGame.Visible = true;
+        }
+
+        private void pictureBox13_Click(object sender, EventArgs e)
+        {
+            this.pictureBoxList.Add(pictureBox13);
+            RefreshTable();
+        }
+
+        public void RefreshTable()
+        {
+            if (pictureBoxList.Count == 1)
+            {
+                this.pictureBoxList[0].Location = new Point(320, 170);
+            }
+            if (pictureBoxList.Count == 2)
+            {
+                this.pictureBoxList[1].BringToFront();
+                this.pictureBoxList[0].Location = new Point(313, 170);
+                this.pictureBoxList[1].Location = new Point(327, 170);
+            }
+            if (pictureBoxList.Count == 3)
+            {
+                this.pictureBoxList[1].BringToFront();
+                this.pictureBoxList[2].BringToFront();
+                this.pictureBoxList[0].Location = new Point(306, 170);
+                this.pictureBoxList[1].Location = new Point(320, 170);
+                this.pictureBoxList[2].Location = new Point(334, 170);
+            }
+            if (pictureBoxList.Count == 4)
+            {
+                this.pictureBoxList[1].BringToFront();
+                this.pictureBoxList[2].BringToFront();
+                this.pictureBoxList[3].BringToFront();
+                this.pictureBoxList[0].Location = new Point(299, 170);
+                this.pictureBoxList[1].Location = new Point(313, 170);
+                this.pictureBoxList[2].Location = new Point(327, 170);
+                this.pictureBoxList[3].Location = new Point(341, 170);
+            }
+            if (pictureBoxList.Count == 5)
+            {
+                this.pictureBoxList[1].BringToFront();
+                this.pictureBoxList[2].BringToFront();
+                this.pictureBoxList[3].BringToFront();
+                this.pictureBoxList[4].BringToFront();
+                this.pictureBoxList[0].Location = new Point(292, 170);
+                this.pictureBoxList[1].Location = new Point(306, 170);
+                this.pictureBoxList[2].Location = new Point(320, 170);
+                this.pictureBoxList[3].Location = new Point(336, 170);
+                this.pictureBoxList[4].Location = new Point(350, 170);
+            }
+            if (pictureBoxList.Count == 6)
+            {
+                this.pictureBoxList[1].BringToFront();
+                this.pictureBoxList[2].BringToFront();
+                this.pictureBoxList[3].BringToFront();
+                this.pictureBoxList[4].BringToFront();
+                this.pictureBoxList[5].BringToFront();
+                this.pictureBoxList[0].Location = new Point(285, 170);
+                this.pictureBoxList[1].Location = new Point(299, 170);
+                this.pictureBoxList[2].Location = new Point(313, 170);
+                this.pictureBoxList[3].Location = new Point(327, 170);
+                this.pictureBoxList[4].Location = new Point(341, 170);
+                this.pictureBoxList[5].Location = new Point(355, 170);
+            }
+        }
+
+        private void pictureBox12_Click(object sender, EventArgs e)
+        {
+            this.pictureBoxList.Add(pictureBox12);
+            RefreshTable();
+        }
+
+        private void pictureBox11_Click(object sender, EventArgs e)
+        {
+            this.pictureBoxList.Add(pictureBox11);
+            RefreshTable();
+        }
+
+        private void pictureBox10_Click(object sender, EventArgs e)
+        {
+            this.pictureBoxList.Add(pictureBox10);
+            RefreshTable();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            this.pictureBoxList.Add(pictureBox4);
+            RefreshTable();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.pictureBoxList.Add(pictureBox2);
+            RefreshTable();
         }
     }
 }
