4a7b0f09f4ef468257062aaef218ab4746d3e3ae
JustUser1410 <JustUser1410@users.noreply.github.com>
2016-01-08 16:03:23 +0100

    Dealer logic
    Finished methods that are used in the beginning of game.
    So, right now Dealer is able to create a deck, shuffle it, deal cards to
    the players and decide who goes first.
    Endpoints are not configured yet.

--- TheFool/GameService/Dealer.cs
+++ TheFool/GameService/Dealer.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TheFool;
 using System.ServiceModel;
+using GameContract;
 //There is no need to copy Card class and enumerations since I added reference
 
 namespace GameService
@@ -13,14 +14,19 @@ namespace GameService
     {
         private List<Card> Deck;
         private List<Card> cardsOnTable;
-        private Proxy prx;
+        private CardSuit TrumpSuit;         //  The suit that defeats others
+        private List<IGameCallback> callbacks;
+        private int player1;
+        private int player2;
 
-        public Dealer()
+        // It takes player IDs at the beginning
+        public Dealer(int player1, int player2) 
         {
-            WSHttpBinding binding = new WSHttpBinding();
-            Uri address = new Uri("http://localhost:8000/clientservice");
-            EndpointAddress endpointAddress = new EndpointAddress(address);
-            prx = new Proxy(binding, endpointAddress);
+            this.player1 = player1;
+            this.player2 = player2;
+            
+            // Set up contracts
+            // TO DO
 
             this.Deck = new List<Card>();
             this.cardsOnTable = new List<Card>();
@@ -52,17 +58,73 @@ namespace GameService
 
         private void ShuffleDeck()
         {
-            
+            // Assigns random number to each element in the list and then orders the list
+            var rnd = new Random();
+            this.Deck.OrderBy(item => rnd.Next());
         }
 
         private void DealCards()
-        { }
+        {
+            List<Card> p1 = new List<Card>();
+            List<Card> p2 = new List<Card>();
+            // Make listst of 6 cards for both players
+            while (p1.Count < 6 && p2.Count < 6)
+            {
+                p1.Add(this.Deck[0]);
+                this.Deck.RemoveAt(0);
+                p2.Add(this.Deck[0]);
+                this.Deck.RemoveAt(0);
+            }
+            // Check Trump Suit
+            // It's the last card in the deck
+            this.TrumpSuit = this.Deck[this.Deck.Count - 1].GetSuit();
 
-        private void DecideFirstPlayer()
-        { }
+            // Give cards to players
+            this.callbacks[0].DrawCards(p1);
+            this.callbacks[1].DrawCards(p2);
+
+            // Check, which one is first
+            DecideFirstPlayer(p1, p2);
+        }
+
+        // Right now it assumes that at least one of the players has a Trump Suit
+        // This will have to be fixed
+        private void DecideFirstPlayer(List<Card> p1, List<Card> p2)
+        {
+            Card p1Smallest = null;
+            Card p2Smallest = null;
+            // Find smallest Trump of Player1
+            for (int i = 0; i < 6; i++)
+                if (p1[i].GetSuit() == this.TrumpSuit)
+                    for (int j = i; j < 6; i++)
+                        if (p1[j].GetSuit() == this.TrumpSuit)
+                            if (p1[i].GetValue() > p1[j].GetValue())
+                                p1Smallest = p1[j];
+                            else
+                                p1Smallest = p1[i];
+            // Find smallest Trump of Player2
+            for (int i = 0; i < 6; i++)
+                if (p2[i].GetSuit() == this.TrumpSuit)
+                    for (int j = i; j < 6; i++)
+                        if (p2[j].GetSuit() == this.TrumpSuit)
+                            if (p2[i].GetValue() > p2[j].GetValue())
+                                p2Smallest = p2[j];
+                            else
+                                p2Smallest = p2[i];
+            // Decide who takes the first turn
+            // Anounce that player to everybody
+            if (p1Smallest.GetValue() < p2Smallest.GetValue())
+                foreach(IGameCallback item in callbacks)
+                    item.PlayerTurn(player1);
+            else
+                foreach (IGameCallback item in callbacks)
+                    item.PlayerTurn(player2);
+        }
 
         private void GameEnds()
-        { }
+        { 
+
+        }
 
     }
 }
