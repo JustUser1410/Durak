9ecc896c1b09d262eed0c72e601e0c711397c9d4
Ashish Kumar <ashish@ashish.co>
2016-01-27 16:42:52 +0100

    Revert "Delete IChat and IGame from GameContract"
    This reverts commit eae92b1a30f20cd3f169d2f98bd915f77eb172d2.

---
+++ TheFool/GameContract/IChat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ChatService
+{
+    [ServiceContract(Namespace = "DurakService", CallbackContract = typeof(IChatCallback))]
+    public interface IChat
+    {
+        [OperationContract(IsOneWay = true)]
+        void StartChatSession(string player);
+
+        [OperationContract(IsOneWay = true)]
+        void PostChatMessage(string message, string postername, string opponent);
+
+    }
+
+    [ServiceContract(Namespace = "BattleShipService")]
+    public interface IChatCallback
+    {
+        [OperationContract]
+        void UpdateChatMessages(string message, string playername);
+    }
+}
---
+++ TheFool/GameContract/IGame.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using TheFool;
+
+namespace GameContract
+{
+    [ServiceContract(Namespace = "GameContract")]
+    public interface IGame
+    {
+        //Following two could be merged into PlayCard(Card c)
+        [OperationContract(IsOneWay = false)]
+        bool Attack(int playerID, Card c);
+
+        /// <summary>
+        /// Notifies that 
+        /// </summary>
+        [OperationContract(IsOneWay = true)]
+        void EndAttack(int playerID);
+
+        [OperationContract(IsOneWay = false)]
+        bool Defend(int playerID, Card c);
+
+        /// <summary>
+        /// Tells service that user wants to surrender
+        /// </summary>
+        [OperationContract(IsOneWay = true)]
+        void Surrender(int playerID);
+
+        /// <summary>
+        /// Sends request to play against random player
+        /// </summary>
+        [OperationContract(IsOneWay = true)]
+        void PlayRandom(int playerID);
+
+        /// <summary>
+        /// Tells service that player with specified ID wants
+        /// to join game with specified token.
+        /// </summary>
+        /// <param name="token">desired token that is necessary for somebody to join</param>
+        /// <param name="playerID">player that wants to Host</param>
+        [OperationContract(IsOneWay = true)]
+        void HostGame(int token, int playerID);
+
+        /// <summary>
+        /// Tells service that player with specified ID wants
+        /// to join game with specified token.
+        /// </summary>
+        /// <param name="token">token that game room was hosted with</param>
+        /// <param name="playerID">player that wants to Join</param>
+        /// <returns>Returns type of error (NONE if there is no error)</returns>
+        [OperationContract(IsOneWay = false)]
+        Errors JoinGame(int token, int playerID);
+
+        /// <summary>
+        /// Whenever user is not able to defend, he must take cards off the table
+        /// and put them in his hand
+        /// </summary>
+        /// <returns></returns>
+        [OperationContract(IsOneWay = false)]
+        List<Card> TakeCards(int playerID);
+
+        /// <summary>
+        /// Notifies dealer that player is out of cards for this move
+        /// </summary>
+        [OperationContract(IsOneWay = true)]
+        void OutOfCards(int playerID); 
+    }
+}
