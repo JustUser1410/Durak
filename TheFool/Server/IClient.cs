353437937ea3f2ce7ffb9a95d7a47279b64d8827
Ashish Kumar <ashish@ashish.co>
2016-01-28 01:47:17 +0100

    Basic working service

---
+++ TheFool/Server/IClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace Server
+{
+    public interface IClient
+    {
+        [OperationContract(IsOneWay = true)]
+        void startGame();
+
+        [OperationContract(IsOneWay = true)]
+        void startTurn(List<Card> cardsOnDeck);
+
+        [OperationContract(IsOneWay = true)]
+        void endGame();
+
+        [OperationContract(IsOneWay = true)]
+        void drawCards(List<Card> cards);
+    }
+}
---
+++ TheFool/Server/IService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace Server
+{
+    [ServiceContract(Namespace = "Server", SessionMode = SessionMode.Required, CallbackContract = typeof(IClient))]
+    public interface IService
+    {
+        [OperationContract(IsOneWay = false)]
+        int joinGame();
+
+        [OperationContract(IsOneWay = true)]
+        void play(int playerID, Card card);
+    }
+}
